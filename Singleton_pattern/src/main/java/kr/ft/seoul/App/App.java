422677fdeea7519c8c816559bafd26c0a9bd92dd
djeeee1272@gmail.com <djeeee1272@gmail.com>
2023-02-07 16:44:35 +0900

    Singleton / Decorator 패턴 디렉토리 업로드

---
+++ Singleton_pattern/src/main/java/kr/ft/seoul/App/App.java
@@ -0,0 +1,12 @@
+package kr.ft.seoul.App;
+
+/*
+ * This is a template project
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("this is a template project");
+    }
+}
