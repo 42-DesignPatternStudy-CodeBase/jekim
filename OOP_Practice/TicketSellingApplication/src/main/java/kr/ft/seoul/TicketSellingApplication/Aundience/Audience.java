e39df639345f47350afb1f4d731b24fbbc046909
djeeee1272@gmail.com <djeeee1272@gmail.com>
2023-02-14 23:59:00 +0900

    Obejct 예제코드 디렉토리 추가

---
+++ OOP_Practice/TicketSellingApplication/src/main/java/kr/ft/seoul/TicketSellingApplication/Aundience/Audience.java
@@ -0,0 +1,15 @@
+package kr.ft.seoul.TicketSellingApplication.Audience;
+
+import kr.ft.seoul.TicketSellingApplication.Bag.Bag;
+
+public class Audience {
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+}
