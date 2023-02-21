235f68d9de4681cb25c7372bd8af00b3403df172
djeeee1272@gmail.com <djeeee1272@gmail.com>
2023-02-21 11:30:30 +0900

    Iterator_pattern : project init

---
+++ Design_Pattern/Iterator_pattern/src/main/java/kr/ft/seoul/IteratorExample/App.java
@@ -0,0 +1,13 @@
+package kr.ft.seoul.IteratorExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
